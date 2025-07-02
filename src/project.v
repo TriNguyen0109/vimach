d3a135752cde0805bca5f209e30571e56db3630d
TriNguyen0109 <117927714+TriNguyen0109@users.noreply.github.com>
2025-07-02 15:24:13 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,6 +1,14 @@
 wire reset =~rst_n;
-assign ulo_out = 0;
+assign uio_out = 0;
 assign uio_oe = 0;
 wire_unuse = &(ena,uio_in,1'b0);
 
-always @(poss)
+ always @(posedge clk or posedge reset) begin
+  if (reset) begin
+    uo_out <= 0;
+  end else begin
+    uo_out[3:0] <= ui_in[7:4] + ui_in[3:0];
+    uo_out[7:4] <= 0;
+  end
+ end
+endmodule
