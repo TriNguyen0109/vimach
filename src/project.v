87a0125949a607849806f51f60366b63a90e2133
TriNguyen0109 <117927714+TriNguyen0109@users.noreply.github.com>
2025-07-03 08:20:13 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -12,16 +12,16 @@ module tt_um_asiclab_example (
 );
 
 wire reset = ~rst_n;
-assign uio_out = 8'b0;
-assign uio_oe = 8'b0;
-wire unuse = &{ena,uio_in,1'b0};
+assign uio_out = 0;
+assign uio_oe = 0;
+wire _unuse = &{ena,uio_in,1'b0};
 
  always @(posedge clk or posedge reset) begin
   if (reset) begin
-    uo_out <= 8'b0;
+    uo_out <= 0;
   end else begin
     uo_out[3:0] <= ui_in[7:4] + ui_in[3:0];
-      uo_out[7:4] <= 4'b0000;
+      uo_out[7:4] <= 0;
   end
  end
 endmodule
